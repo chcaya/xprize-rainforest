7ee3e59949f8c3282b3794842c6872321a125639
DaoudPiracha <daoud.piracha@mail.mcgill.ca>
2024-06-20 19:02:05 +0500

    sped up download of each species by using asyncio

--- data/gbif/api_utils.py
+++ data/gbif/api_utils.py
@@ -26,11 +26,12 @@ def fetch_species_details(species_id: str) -> Dict:
     """Fetch detailed information for a species using its GBIF ID."""
     return fetch_data(f"{API_GBIF_SPECIES_DETAIL}{species_id}")
 
-
-def fetch_occurrences_by_taxon(taxon_keys: Dict[str, str], limit: int = 100) -> List[Dict]:
+# todo: se
+def fetch_occurrences_by_taxon(taxon_keys: Dict[str, str], limit: int = 20) -> List[Dict]:
     """Fetch occurrences for given taxon keys."""
     params = {
         'mediaType': 'StillImage',
+        'quality': 'RESEARCH',
         'limit': limit
     }
     # Merge the specific taxon keys into the params dictionary
@@ -39,26 +40,28 @@ def fetch_occurrences_by_taxon(taxon_keys: Dict[str, str], limit: int = 100) -> 
     return data.get('results', [])
 
 
-def fetch_research_grade_image_urls(taxon_keys: Dict[str, str]) -> List[str]:
+def fetch_research_grade_image_urls(taxon_keys: Dict[str, str], limit = 20) -> List[str]:
     """Fetch research-grade image URLs from GBIF occurrences."""
-    results = fetch_occurrences_by_taxon(taxon_keys, limit=20)
+    results = fetch_occurrences_by_taxon(taxon_keys, limit=limit)
     image_urls = []
     for result in results:
         if 'media' in result:
             for media in result['media']:
                 if 'identifier' in media:
                     image_urls.append(media['identifier'])
-    return image_urls
+    return image_urls[:limit]
 
 
-# todo: create separate folder for each species/family
-def download_image(image_url: str, output_dir: Path, file_name: str) -> None:
-    """Download an image from a given URL and save it to the specified directory."""
+def download_image(image_url: str, output_dir: Path, folder_name: str, file_name: str) -> None:
+    """Download an image from a given URL and save it to a directory organized by folder name (species or family)."""
+    specific_output_dir = output_dir / folder_name
+    specific_output_dir.mkdir(parents=True, exist_ok=True)
+
     try:
         response = requests.get(image_url)
         response.raise_for_status()
         image = Image.open(BytesIO(response.content))
-        output_path = output_dir / file_name
+        output_path = specific_output_dir / file_name
         image.save(output_path)
         logging.info(f"Image saved to {output_path}")
     except requests.RequestException as e:
--- data/gbif/query_api.py
+++ data/gbif/query_api.py
@@ -1,10 +1,14 @@
 import logging
-from typing import Dict
+import os.path
+from typing import Dict, List
 from pathlib import Path
 from PIL import Image
 from io import BytesIO
 import pandas as pd
 import requests
+import asyncio
+import aiohttp
+import aiofiles
 
 from api_utils import fetch_species_details, fetch_research_grade_image_urls, download_image
 
@@ -25,30 +29,81 @@ def download_images(image_urls: list, output_dir: Path) -> None:
             logging.error(f"Failed to download image: {e}")
 
 
+async def download_image_async(session, url, output_dir, file_name_prefix, index):
+    try:
+        async with session.get(url) as response:
+            response.raise_for_status()  # Ensure we notice bad responses
+            # Read the content
+            image_data = await response.read()
+            image = Image.open(BytesIO(image_data))
+            # Asynchronously save the image using an executor
+            loop = asyncio.get_running_loop()
+            await loop.run_in_executor(None, save_image, image, output_dir, file_name_prefix, index)
+            logging.info(f'Downloaded image {index + 1}')
+    except Exception as e:
+        logging.error(f"Failed to download image {index + 1}: {e}")
+
+
+def save_image(image, output_dir, file_name_prefix, index):
+    """Save an image file synchronously."""
+    with open(output_dir / f'{file_name_prefix}_{index + 1}.jpg', 'wb') as f:
+        image.save(f, format='JPEG')
+
+
+async def download_all_images(image_urls: List[str], output_dir: Path, folder_name: str, file_name_prefix: str) -> None:
+    image_dir = output_dir / folder_name
+    if not os.path.exists(image_dir):
+        os.makedirs(image_dir)
+    async with aiohttp.ClientSession() as session:
+        tasks = [download_image_async(session, url, image_dir, file_name_prefix, i) for i, url in enumerate(image_urls)]
+        await asyncio.gather(*tasks)
+
+
 def build_taxon_keys_dict(row) -> Dict[str, str]:
     """Builds a dictionary of taxon keys from a DataFrame row."""
     keys = ['speciesKey', 'genusKey', 'familyKey', 'orderKey', 'classKey', 'phylumKey', 'kingdomKey']
-    return {key: row[key] for key in keys if key in row}
+    return {key: int(row[key]) for key in keys if key in row}
 
 
+# todo: save metadata of image along with image itself
 def process_species_images(csv_file_path: Path, image_output_dir: Path, num_rows: int = 1) -> None:
+    # read and process csv
     data = pd.read_csv(csv_file_path)
+    float_cols = data.select_dtypes(include='float').columns
+    # drop duplicates based on family key
+    data = data.dropna(subset=['speciesKey'])
+    data = data.drop_duplicates(subset='familyKey', keep='first')
+    data[float_cols] = data[float_cols].fillna(-1).astype(int)
+
     for index, row in data.head(num_rows).iterrows():
 
+        family_key = row["familyKey"]
+        genus_key = row["genusKey"]
         species_key = row['speciesKey']
+
+        folder_name = f'{family_key}_{genus_key}_{species_key}'
+
+        if os.path.exists(image_output_dir / folder_name):
+            logging.info(f"Found existing folder. Skipping downloads for speciesKey {species_key}.")
+            continue
+
         logging.info(f'Processing speciesKey: {species_key}')
         species_details = fetch_species_details(species_key)
         if species_details:
             if verify_and_log_details(index, row, species_details):
                 taxon_keys = build_taxon_keys_dict(row)
-                image_urls = fetch_research_grade_image_urls(taxon_keys)
+                image_urls = fetch_research_grade_image_urls(taxon_keys, limit=10)
 
                 # todo: refactor to separate function
                 if image_urls:
-                    logging.info(f"Found research-grade images for {taxon_keys}: {image_urls}")
-                    for i, url in enumerate(image_urls):
-                        file_name = f"{species_key}_{i + 1}.jpg"
-                        download_image(url, image_output_dir, file_name)
+                    logging.info(f"Found {len(image_urls)} research-grade images for {species_key}: {image_urls}")
+                    asyncio.run(download_all_images(image_urls=image_urls,
+                                                    output_dir=image_output_dir,
+                                                    folder_name=folder_name,
+                                                    file_name_prefix=species_key))
+                    # for i, url in enumerate(image_urls):
+                    #     file_name = f"{species_key}_{i + 1}.jpg"
+                    #     download_image(url, image_output_dir, folder_name, file_name)
                     logging.info(f"Downloaded {len(image_urls)} images for speciesKey {species_key}.")
                 else:
                     logging.warning(f"No research-grade images found for {taxon_keys}.")
@@ -92,8 +147,15 @@ def verify_and_log_details(index, row, species_details):
 # Example usage
 if __name__ == "__main__":
     dir_path = Path('/Users/daoud/PycharmAssets/xprize/')
-    csv_file_name = 'brazil_trees_amazonia_sel_occs.csv'
-    csv_file_path = dir_path / csv_file_name
+    brazil_gbif_species_csv = 'brazil_trees_amazonia_sel_occs.csv'
+    brazil_photos_taxonomy_csv = 'photos_exif_taxo.csv'
+
+    brazil_gbif_species_csv_path = dir_path / brazil_gbif_species_csv
+    brazil_photos_taxonomy_csv_path = dir_path / brazil_photos_taxonomy_csv
+
+    # brazil_gbif_species = pd.read_csv(brazil_gbif_species_csv_path)
+    brazil_photos_taxonomy = pd.read_csv(brazil_photos_taxonomy_csv_path)
+
     image_output_dir = dir_path / 'images/'
 
-    process_species_images(csv_file_path, image_output_dir)
+    process_species_images(brazil_photos_taxonomy_csv_path, image_output_dir, num_rows=100)
