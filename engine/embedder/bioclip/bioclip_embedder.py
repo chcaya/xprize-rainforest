55ff9090ade57a12eae7f984638fe4f351c0a512
DaoudPiracha <daoud.piracha@mail.mcgill.ca>
2024-06-28 17:43:43 +0500

    cleaned up

--- engine/embedder/bioclip/bioclip_embedder.py
+++ engine/embedder/bioclip/bioclip_embedder.py
@@ -2,24 +2,54 @@ import glob
 import os
 
 import pandas as pd
-import torch
 from PIL import Image
 from pathlib import Path
 import open_clip
-import matplotlib.pyplot as plt
+
 import numpy as np
-from sklearn.manifold import TSNE
-from sklearn.neighbors import KNeighborsClassifier
-import seaborn as sns  # For a nicer confusion matrix visualization
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
-from sklearn.preprocessing import LabelEncoder
-from sklearn.svm import SVC
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+from sklearn.manifold import TSNE
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
+import seaborn as sns
+from typing import Tuple, Dict, Any
+
+
+from typing import Tuple, Any, Dict
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import torch
+import torch.optim as optim
+
+
+class SimpleNN(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(SimpleNN, self).__init__()
+        self.fc = nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        return self.fc(x)
+
+
+class TwoLayerNN(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(TwoLayerNN, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
 
 
 def load_and_preprocess_images(image_paths, preprocess):
@@ -80,14 +110,23 @@ def plot_embeddings(embeddings, labels, markers):
     plt.grid(True)
     plt.show()
 
-def plot_confusion_matrix(confusion_matrix, class_names):
-    # Plotting the confusion matrix
+
+def plot_confusion_matrix(cm: np.ndarray, class_names: np.ndarray) -> None:
+    """
+    Plot the confusion matrix using seaborn heatmap.
+
+    Parameters:
+    cm (np.ndarray): Confusion matrix.
+    class_names (np.ndarray): Array of class names.
+    """
     plt.figure(figsize=(10, 7))
-    sns.heatmap(confusion_matrix, annot=True, fmt="d", cmap='Blues', xticklabels=class_names, yticklabels=class_names)
+    sns.heatmap(cm, annot=True, fmt="d", cmap='Blues', xticklabels=class_names, yticklabels=class_names)
     plt.title('Confusion Matrix')
     plt.ylabel('True Label')
     plt.xlabel('Predicted Label')
     plt.show()
+
+
 
 
 def get_folders(directory):
@@ -107,16 +146,44 @@ def get_taxon_key_from_df(file_name, df, key='genusKey'):
     return int(search_row.iloc[0][key])
 
 
-def train_downstream_model(embeddings, labels, classifier):
-    # knn model
+def preprocess_data(
+    embeddings: np.ndarray,
+    labels: np.ndarray,
+    test_size: float = 0.2,
+    random_state: int = 42
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, LabelEncoder]:
     label_encoder = LabelEncoder()
-
-    # Fit label encoder and return encoded labels
     encoded_labels = label_encoder.fit_transform(labels)
 
-    # Split the data into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(embeddings, encoded_labels, test_size=0.4, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        embeddings, encoded_labels,
+        test_size=test_size,
+        stratify=encoded_labels,
+        random_state=random_state
+    )
 
+    return X_train, X_test, y_train, y_test, label_encoder
+
+def convert_to_tensor(
+    X_train: np.ndarray,
+    X_test: np.ndarray,
+    y_train: np.ndarray,
+    y_test: np.ndarray
+) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+    X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
+    X_test_tensor = torch.tensor(X_test, dtype=torch.float32)
+    y_train_tensor = torch.tensor(y_train, dtype=torch.long)
+    y_test_tensor = torch.tensor(y_test, dtype=torch.long)
+
+    return X_train_tensor, X_test_tensor, y_train_tensor, y_test_tensor
+
+def train_downstream_model(X_train: np.ndarray,
+                           X_test: np.ndarray,
+                           y_train: np.ndarray,
+                           y_test: np.ndarray,
+                           classifier: Any,
+                           label_encoder: LabelEncoder,
+    ) -> Tuple[Any, str]:
     classifier.fit(X_train, y_train)
 
     y_pred = classifier.predict(X_test)
@@ -128,50 +195,115 @@ def train_downstream_model(embeddings, labels, classifier):
 
     # Generate confusion matrix
     cm = confusion_matrix(y_test, y_pred)
-    class_names = label_encoder.inverse_transform(np.unique(encoded_labels))
+    class_names = np.unique(y_test)
+    class_names = label_encoder.inverse_transform(class_names)
     plot_confusion_matrix(cm, class_names)
 
+    return classifier, classification_report(y_test, y_pred)
+
+def train_downstream_model_nn(
+        X_train: np.ndarray,
+        X_test: np.ndarray,
+        Y_train: np.ndarray,
+        Y_test: np.ndarray,
+        config: Dict[str, Any],
+        label_encoder: LabelEncoder,
+) -> Tuple[Any, float, str]:
+    """
+    Train a simple neural network on the provided embeddings and labels.
+
+    Parameters:
+    X_train (torch.Tensor): The training embeddings.
+    X_test (torch.Tensor): The test embeddings.
+    y_train (torch.Tensor): The training labels.
+    y_test (torch.Tensor): The test labels.
+    config (dict): Configuration dictionary with keys 'test_size', 'random_state',
+                   'hidden_dim', 'num_epochs', and 'learning_rate'.
+
+    Returns:
+    Tuple[TwoLayerNN, float, str]: Trained model, overall accuracy, and classification report.
+    """
+    X_train, X_test, Y_train, Y_test = convert_to_tensor(X_train, X_test, Y_train, Y_test)
+
+    # Set device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Initialize the model, loss function, and optimizer
+    input_dim = X_train.shape[1]
+    output_dim = len(np.unique(Y_train.cpu().numpy()))
+    model = TwoLayerNN(input_dim, config['hidden_dim'], output_dim).to(device)
+    criterion = config['loss']
+    optimizer = optim.Adam(model.parameters(), lr=config['learning_rate'])
+
+    # Move data to device
+    X_train = X_train.to(device)
+    Y_train = Y_train.to(device)
+    X_test = X_test.to(device)
+
+    # Training loop
+    for epoch in range(config['num_epochs']):
+        model.train()
+        optimizer.zero_grad()
+        outputs = model(X_train)
+        loss = criterion(outputs, Y_train)
+        print (epoch, loss)
+        loss.backward()
+        optimizer.step()
+
+    # Evaluate the model
+    model.eval()
+    with torch.no_grad():
+        y_pred_nn = model(X_test)
+        y_pred_nn = torch.argmax(y_pred_nn, axis=1).cpu().numpy()
+
+    # Confusion matrix
+    cm_nn = confusion_matrix(Y_test.cpu().numpy(), y_pred_nn)
+    class_names = np.unique(Y_test.cpu().numpy())
+    class_names = label_encoder.inverse_transform(class_names)
+
+    # Plotting the confusion matrix
+    plot_confusion_matrix(cm_nn, class_names)
+
+    # Calculate accuracy and report per class
+    overall_accuracy = accuracy_score(Y_test.cpu().numpy(), y_pred_nn)
+    class_report = classification_report(Y_test.cpu().numpy(), y_pred_nn)
+
+    print("Overall accuracy:", overall_accuracy)
+    print("Classification report per class:")
+    print(class_report)
+
+    return model, overall_accuracy, class_report
 
 
-
-class SimpleNN(nn.Module):
-    def __init__(self, input_dim, output_dim):
-        super(SimpleNN, self).__init__()
-        self.fc = nn.Linear(input_dim, output_dim)
-
-    def forward(self, x):
-        return self.fc(x)
-
-
-class TwoLayerNN(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim):
-        super(TwoLayerNN, self).__init__()
-        self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_dim, output_dim)
-
-    def forward(self, x):
-        x = self.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
 
 
 if __name__ == "__main__":
 
-    # constants
-    make_plot = True
+    # visualize clusters for more interpretability
+    make_plot = False
+
+    # neural network train config
+    config = {
+        'test_size': 0.2,
+        'random_state': 42,
+        'hidden_dim': 100,
+        'num_epochs': 100,
+        'learning_rate': 0.001,
+        'loss': nn.CrossEntropyLoss()
+
+    }
 
     # Define paths
     dir_path = Path('/Users/daoud/PycharmAssets/xprize/')
     brazil_photos_taxonomy_path = dir_path / 'photos_exif_taxo.csv'
 
-    # Load data
-    brazil_photos_taxonomy = pd.read_csv(brazil_photos_taxonomy_path)
-    filename_and_key = brazil_photos_taxonomy[['fileName', 'familyKey', 'genusKey', 'speciesKey']]
-
     # File search pattern
     folder_glob_search_str = dir_path / "dji/zoomed_out/cropped/*"
     folders = glob.glob(str(folder_glob_search_str))
+
+    # Load data
+    brazil_photos_taxonomy = pd.read_csv(brazil_photos_taxonomy_path)
+    filename_and_key = brazil_photos_taxonomy[['fileName', 'familyKey', 'genusKey', 'speciesKey']]
 
     # Load the model
     model_path = dir_path / "open_clip_pytorch_model.bin"
@@ -184,7 +316,7 @@ if __name__ == "__main__":
     # Initialization
     all_embeddings, labels, markers = [], [], []
 
-    # Process images
+    # Process images to create BioCLIP embeddings, labels
     for idx, folder in enumerate(folders):
         image_paths = glob.glob(str(dir_path / folder / '*'))
         filenames = [os.path.basename(image_path) for image_path in image_paths]
@@ -204,58 +336,18 @@ if __name__ == "__main__":
     all_embeddings = np.vstack(all_embeddings)
     # todo: save embeddings for lazy loading
 
+    # visualize T-SNE of BioCLIP embeddings to see presence of clusters
     if make_plot:
         plot_embeddings(all_embeddings, labels, markers)
+
+    labels = np.vstack(labels)
 
     # downstream prediction
     knn = KNeighborsClassifier(n_neighbors=10)
     svc = SVC(kernel='linear')  # You can change the kernel type based on your needs
 
-    train_downstream_model(all_embeddings, labels, classifier=svc)
+    X_train, X_test, Y_train, Y_test, label_encoder = preprocess_data(all_embeddings, labels, test_size=config['test_size'], random_state=42)
+    model_svc, classification_report_svc = train_downstream_model(X_train, X_test, Y_train, Y_test, classifier=svc, label_encoder=label_encoder)
+    model_nn, acc_nn, classification_report_nn = train_downstream_model_nn(X_train, X_test, Y_train, Y_test, config=config, label_encoder=label_encoder)
 
-# # Setup the network
-#     input_dim = X_train.shape[1]
-#     output_dim = len(np.unique(y_train))
-#     model = SimpleNN(input_dim, output_dim).to(device)
-#     hidden_dim = 100
-#     model = TwoLayerNN(input_dim, hidden_dim, output_dim)
-#     # Loss and optimizer
-#     criterion = nn.CrossEntropyLoss()
-#     optimizer = optim.Adam(model.parameters(), lr=0.001)
-#
-#
-#     # Convert data to tensors
-#     X_train_tensor = torch.tensor(X_train, dtype=torch.float).to(device)
-#     y_train_tensor = torch.tensor(y_train, dtype=torch.long).to(device)
-#     X_test_tensor = torch.tensor(X_test, dtype=torch.float).to(device)
-#
-#     # Training loop
-#     for epoch in range(100):  # number of epochs can be adjusted
-#         print (epoch)
-#         optimizer.zero_grad()
-#         outputs = model(X_train_tensor)
-#         loss = criterion(outputs, y_train_tensor)
-#         loss.backward()
-#         optimizer.step()
-#
-#     # Evaluate the model
-#     with torch.no_grad():
-#         y_pred_nn = model(X_test_tensor)
-#         y_pred_nn = torch.argmax(y_pred_nn, axis=1).cpu().numpy()
-#
-#     # Confusion matrix
-#     cm_nn = confusion_matrix(y_test, y_pred_nn)
-#     class_names = label_encoder.inverse_transform(np.unique(encoded_labels))
-#
-#     # Plotting the confusion matrix
-#     plt.figure(figsize=(10, 7))
-#     sns.heatmap(cm_nn, annot=True, fmt="d", cmap='Blues', xticklabels=class_names, yticklabels=class_names)
-#     plt.title('Confusion Matrix')
-#     plt.ylabel('True Label')
-#     plt.xlabel('Predicted Label')
-#     plt.show()
-#
-#     # Calculate accuracy and report per class
-#     print("Overall accuracy:", accuracy_score(y_test, y_pred_nn))
-#     print("Classification report per class:")
-#     print(classification_report(y_test, y_pred_nn))
+    #todo: package embedder and trained classifier into one object
